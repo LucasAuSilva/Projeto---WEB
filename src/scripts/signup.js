8961f2dd689face5c401a0b86313044a2870c3ee
LucasAuSilva <silvaaugustolucas@gmail.com>
2020-11-02 18:24:43 -0300

    signup ready, missing localStorage

--- src/scripts/signup.js
+++ src/scripts/signup.js
@@ -1,20 +1,42 @@
 
 const submitButton = document.getElementById("handle-form")
+const inputs = document.getElementsByTagName('input')
 
 submitButton.addEventListener('click', () => {
-    const inputs = [
-        'first-name-error',
-        'last-name-error',
-        'email-error',
-        'password-error',
-        'password-confirm-error'
-    ]
-    let cont = inputs.length
-    for (var x = 0; cont > x; x++) {
-        let input = document.getElementsByClassName(inputs[x]).value
-        if (input == null) {
-            let p = document.getElementsByClassName(inputs[x])
-            p.innerHTML('fodase')
+    const cont = inputs.length
+    let empty = false
+    conf = true
+    for (let x = 0; cont > x; x++) {
+        if (inputs[x].value != "") {
+            let text = document.getElementsByTagName('p')[x + 1]
+            text.style.display = 'none'
+        } else {
+            let text = document.getElementsByTagName('p')[x + 1]
+            text.style.display = 'initial'
+            empty = true
         }
     }
+    if (inputs[3].value != inputs[4].value) {
+        console.log(inputs[3].value)
+        let overwrite = document.getElementById('confirm')
+        overwrite.style.display = 'initial'
+        overwrite.innerHTML = "<strong> Campo Obrigatório, Senhas devem ser iguais! </strong>"
+        conf = false
+    } else {
+        let overwrite = document.getElementById('confirm')
+        overwrite.style.display = 'none'
+    }
+    if (!empty && conf) {
+        let register = []
+        for (x = 0; cont > x; x++) {
+            register.push(inputs[x].value)
+        }
+        for (let i = 0; i < localStorage.length; i++) {
+            dado = JSON.parse(localStorage.getItem(i))
+            if (register == dado) {
+                window.alert('Usuario ja cadastrado')
+            }
+        }
+    }
+
 })
