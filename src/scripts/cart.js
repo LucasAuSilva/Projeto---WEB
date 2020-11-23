c333ee719b85774a0e86a3784316ee757ca1b4db
Guilherme Illescas <gui.illescas@gmail.com>
2020-11-22 22:09:29 -0300

    Fix logic to block not logged user and responsivity

--- src/scripts/cart.js
+++ src/scripts/cart.js
@@ -1,175 +1,170 @@
 // **** PRODUCTS - JAVASCRIPT **** //
 
 const logado = localStorage.getItem('logado')
-const addButton = document.querySelectorAll('[name="add"]')
+const addButton = document.getElementsByClassName('add')
 const category = document.querySelectorAll('.option-block')
 const cont = document.querySelector("#contagem")
 const products = [{
-        name: "Apple Watch",
-        tag: "apple-watch-sell.jpeg",
-        price: 4059.00,
-        qtd: 0
-    },
-    {
-        name: "Thunder Black",
-        tag: "thunder-black.jpg",
-        price: 1899.90,
-        qtd: 0
-    },
-    {
-        name: "Curren M2",
-        tag: "black-curren-m2.jpg",
-        price: 710.09,
-        qtd: 0
-    }
+    name: "Apple Watch",
+    tag: "apple-watch-sell.jpeg",
+    price: 4059.00,
+    qtd: 0
+  },
+  {
+    name: "Thunder Black",
+    tag: "thunder-black.jpg",
+    price: 1899.90,
+    qtd: 0
+  },
+  {
+    name: "Curren M2",
+    tag: "black-curren-m2.jpg",
+    price: 710.09,
+    qtd: 0
+  }
 ]
 
 for (let x = 0; x < category.length; x++) {
-    category[x].addEventListener("click", () => {
-        categoryOpen(x, products)
-    })
+  category[x].addEventListener("click", () => {
+    categoryOpen(x, products)
+  })
 }
 
 for (let i = 0; i < addButton.length; i++) {
-    addButton[i].addEventListener("click", () => {
-        if (logado != null && logado[0]) {
-            contCart()
-            insideCart(products[i])
-            totalPrice(products[i])
-        } else {
-            window.alert("VOCE PRECISA ESTAR LOGADO")
-            window.location.href = "../SignIn/index.html"
-        }
-    })
+  addButton[i].addEventListener("click", () => {
+    if (logado != null && logado[0]) {
+      contCart()
+      insideCart(products[i])
+      totalPrice(products[i])
+    } else {
+      window.alert("VOCE PRECISA ESTAR LOGADO")
+      window.location.href = "../SignIn/index.html"
+    }
+  })
 }
 
 displayCart()
 loadCartCount()
 
 function contCart() {
-    let contItens = parseInt(localStorage.getItem('contCart'))
+  let contItens = parseInt(localStorage.getItem('contCart'))
 
-    if (contItens) {
-        localStorage.setItem('contCart', contItens + 1)
-        cont.innerHTML = contItens + 1
-    } else {
-        cont.style.display = "initial"
-        localStorage.setItem('contCart', 1)
-        cont.innerHTML = 1
-    }
+  if (contItens) {
+    localStorage.setItem('contCart', contItens + 1)
+    cont.innerHTML = contItens + 1
+  } else {
+    cont.style.display = "initial"
+    localStorage.setItem('contCart', 1)
+    cont.innerHTML = 1
+  }
 }
 
 function loadCartCount() {
-    let contItens = localStorage.getItem('contCart')
+  let contItens = localStorage.getItem('contCart')
 
-    if (contCart) {
-        cont.innerHTML = contItens
-        cont.style.display = "initial"
-    }
+  if (contCart) {
+    cont.innerHTML = contItens
+    cont.style.display = "initial"
+  }
 }
 
 function insideCart(product) {
-    let cart = JSON.parse(localStorage.getItem("cart"))
-    let cartItens = cart
+  let cart = JSON.parse(localStorage.getItem("cart"))
+  let cartItens = cart
 
-    if (cartItens == null) {
-        product.qtd = 1
-        cartItens = {
-            [product.tag]: product
-        }
-    } else if (cartItens[product.tag] == undefined) {
-        product.qtd = 1
-        cartItens = {
-            ...cartItens,
-            [product.tag]: product
-        }
-    } else {
-        cartItens[product.tag].qtd += 1;
+  if (cartItens == null) {
+    product.qtd = 1
+    cartItens = {
+      [product.tag]: product
     }
+  } else if (cartItens[product.tag] == undefined) {
+    product.qtd = 1
+    cartItens = {
+      ...cartItens,
+      [product.tag]: product
+    }
+  } else {
+    cartItens[product.tag].qtd += 1;
+  }
 
-    localStorage.setItem("cart", JSON.stringify(cartItens))
+  localStorage.setItem("cart", JSON.stringify(cartItens))
 }
 
 function totalPrice(product) {
-    let totalPrice = parseInt(localStorage.getItem('total'))
+  let totalPrice = parseInt(localStorage.getItem('total'))
 
-    if (totalPrice) {
-        localStorage.setItem('total', parseFloat(totalPrice) + product.price)
-    } else {
-        localStorage.setItem('total', product.price)
-    }
+  if (totalPrice) {
+    localStorage.setItem('total', parseFloat(totalPrice) + product.price)
+  } else {
+    localStorage.setItem('total', product.price)
+  }
 }
 
 function categoryOpen(position, products) {
-    if (products.length > 3) {
-        products = products.splice(3,3)
-    }
-    if (position == 0) {
-        products.push({
-            name: "Apple Watch",
-            tag: "apple-watch-sell.jpeg",
-            price: 4059.00,
-            qtd: 0
-        },
-        {
-            name: "Samsung SmartWatch",
-            tag: "samsumg-watch.jpg",
-            price: 899.00,
-            qtd: 0
-        },
-        {
-            name: "Samsumg SmartWatch",
-            tag: "miband.jpg",
-            price: 300.00,
-            qtd: 0
-        })
-    } else if (position == 1) {
-        products.push({
-            name: "Geneva Romans",
-            tag: "geneva-watch.jpg",
-            price: 2339.00,
-            qtd: 0
-        },
-        {
-            name: "Thunder Black",
-            tag: "thunder-black.jpg",
-            price: 1899.00,
-            qtd: 0
-        },
-        {
-            name: "Curren Olt Town",
-            tag: "curren-old-town.jpg",
-            price: 3720.00,
-            qtd: 0
-        })
-    } else if (position == 2) {
-        products.push({
-            name: "X-Games Master Boy",
-            tag: "digital-1.jpg",
-            price: 349.00,
-            qtd: 0
-        },
-        {
-            name: "Skmei Sport",
-            tag: "digital-2.jpg",
-            price: 189.00,
-            qtd: 0
-        })
-    }
+  if (products.length > 3) {
+    products = products.splice(3, 3)
+  }
+  if (position == 0) {
+    products.push({
+      name: "Apple Watch",
+      tag: "apple-watch-sell.jpeg",
+      price: 4059.00,
+      qtd: 0
+    }, {
+      name: "Samsung SmartWatch",
+      tag: "samsumg-watch.jpg",
+      price: 899.00,
+      qtd: 0
+    }, {
+      name: "Samsumg SmartWatch",
+      tag: "miband.jpg",
+      price: 300.00,
+      qtd: 0
+    })
+  } else if (position == 1) {
+    products.push({
+      name: "Geneva Romans",
+      tag: "geneva-watch.jpg",
+      price: 2339.00,
+      qtd: 0
+    }, {
+      name: "Thunder Black",
+      tag: "thunder-black.jpg",
+      price: 1899.00,
+      qtd: 0
+    }, {
+      name: "Curren Olt Town",
+      tag: "curren-old-town.jpg",
+      price: 3720.00,
+      qtd: 0
+    })
+  } else if (position == 2) {
+    products.push({
+      name: "X-Games Master Boy",
+      tag: "digital-1.jpg",
+      price: 349.00,
+      qtd: 0
+    }, {
+      name: "Skmei Sport",
+      tag: "digital-2.jpg",
+      price: 189.00,
+      qtd: 0
+    })
+  }
 }
 
 
 // **** CART  - JAVASCRIPT **** //
 
 function displayCart() {
-    let cart = JSON.parse(localStorage.getItem('cart'))
-    let totalPrice = JSON.parse(localStorage.getItem('total'))
-    let productContainer = document.querySelector(".products")
+  let cart = JSON.parse(localStorage.getItem('cart'))
+  let totalPrice = JSON.parse(localStorage.getItem('total'))
+  let productContainer = document.querySelector(".products")
 
-    if (cart && productContainer) {
-        productContainer.innerHTML = " ";
-        Object.values(cart).map(item => {
-            productContainer.innerHTML += `
+  if (cart && productContainer) {
+    productContainer.innerHTML = " ";
+    Object.values(cart).map(item => {
+      productContainer.innerHTML += `
             <div class="product">
                 <ion-icon name="close-circle" class=".remove-button"></ion-icon>
                 <img src="../../images/${item.tag}">
@@ -183,9 +178,9 @@ function displayCart() {
                 R$ ${item.qtd * item.price}
             </div>
             `
-        })
+    })
 
-        productContainer.innerHTML += `
+    productContainer.innerHTML += `
             <div class="basketTotalContainer">
                 <h4 class="basketTotalTitle">
                     Preço Total
@@ -195,11 +190,11 @@ function displayCart() {
                 </h4>
             </div>
         `
-    }
+  }
 }
 
 function rmvQtd(tag) {
-    let cart = JSON.parse(localStorage.getItem('cart'))
+  let cart = JSON.parse(localStorage.getItem('cart'))
 
-    console.log(tag)
+  console.log(tag)
 }
